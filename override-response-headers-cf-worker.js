1bcd9ec73430fdd839b409aef291086f6ec3f321
Praveen A <pravinady@live.com>
2025-08-22 16:46:17 -0700

    updated cf worker

--- override-response-headers-cf-worker.js
+++ override-response-headers-cf-worker.js
@@ -9,10 +9,12 @@
  */
 
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(handleRequest(event.request));
 })
 
 async function handleRequest(request) {
+  const WORKER_NAME = 'pro-serv-auth0-showcase-demo-worker';
+  console.log(`Inside ${WORKER_NAME} `);
 
   // Fetch the response from the origin server.
   const response = await fetch(request);
@@ -20,9 +22,13 @@ async function handleRequest(request) {
   // Create a new mutable response object.
   const newResponse = new Response(response.body, response);
 
-  // Remove the specified headers.
+  // Remove the specified headers so auth0 universal login can be loaded in iFrame
   newResponse.headers.delete('Content-Security-Policy');
   newResponse.headers.delete('X-Frame-Options');
+
+  //Add additional headers so passkeys can work in iFrames
+  //Below headers will allow iframe's to call webauthn api
+  newResponse.headers.set('Permissions-Policy', 'publickey-credentials-get=(*), publickey-credentials-create=(*)');  
   
   // Return the modified response.
   return newResponse;
